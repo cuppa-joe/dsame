1cc1227234aacd478c9799483d25f8b669e86b7d
cuppa-joe <cuppa-joe@xoynq.com>
2015-05-17 15:43:31 -0500

    Update script

--- scripts/source-rtl-fm-multimon-ng.bat
+++ scripts/source-rtl-fm-multimon-ng.bat
@@ -2,4 +2,4 @@
 set PPM=0
 set FREQ=162.500M
 set GAIN=42
-rtl_fm -f %FREQ%-M fm -s 22050 -E dc -p %PPM% -g %GAIN%  -|  multimon-ng -t raw -a EAS -f alpha -
+rtl_fm -f %FREQ%-M fm -s 22050 -E dc -p %PPM% -g %GAIN%  -|  multimon-ng -t raw -a EAS -
