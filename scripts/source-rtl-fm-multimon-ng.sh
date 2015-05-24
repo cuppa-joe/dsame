8ed98397a8b28400b09d5c7446e28a01fe02cfba
cuppa-joe <cuppa-joe@xoynq.com>
2015-05-23 23:31:07 -0500

    Missing M
    Thanks, Alex

--- scripts/source-rtl-fm-multimon-ng.sh
+++ scripts/source-rtl-fm-multimon-ng.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo INPUT: rtl_fm Device 1>&2
 PPM=0
-FREQ=162.500
+FREQ=162.500M
 GAIN=42
 rtl_fm -f ${FREQ} -M fm -s 22050 -E dc -p ${PPM} -g ${GAIN}  -|  multimon-ng -t raw -a EAS /dev/stdin
